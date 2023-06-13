e94d06b2fd765d485ac4735cef0aca3377db7a6c
Krestenub <114006864+krestenub@users.noreply.github.com>
2023-06-13 16:35:13 +0200

    lys!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Assets/PressFToOnOff.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressFToOnOff : MonoBehaviour
+{ public GameObject Flashlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+           
+            if (Flashlight.activeSelf == true)
+            {
+                Flashlight.SetActive(false);
+            }
+            else if(Flashlight.activeSelf == false)
+            {
+              
+                Flashlight.SetActive(true);
+            }
+
+
+        }
+    }
+}
