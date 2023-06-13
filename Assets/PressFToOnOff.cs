5a3b33dd4fd6ab9be12b9914bbafe4c64fba94e2
Krestenub <114006864+krestenub@users.noreply.github.com>
2023-06-13 16:56:44 +0200

    prøv se her

--- Assets/PressFToOnOff.cs
+++ Assets/PressFToOnOff.cs
@@ -4,29 +4,46 @@ using UnityEngine;
 
 public class PressFToOnOff : MonoBehaviour
 { public GameObject Flashlight;
+    public float PowerOnFlashlight;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if (PowerOnFlashlight > 0)
         {
-           
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+
+                if (Flashlight.activeSelf == true)
+                {
+                    Flashlight.SetActive(false);
+                }
+                else if (Flashlight.activeSelf == false)
+                {
+
+                    Flashlight.SetActive(true);
+                }
+
+
+            }
             if (Flashlight.activeSelf == true)
             {
-                Flashlight.SetActive(false);
-            }
-            else if(Flashlight.activeSelf == false)
-            {
-              
-                Flashlight.SetActive(true);
-            }
+                PowerOnFlashlight -= 1 * Time.deltaTime;
 
-
+            }
         }
+        else
+        {
+            Flashlight.SetActive(false);
+        }
+
+        print(PowerOnFlashlight);
+
     }
 }
