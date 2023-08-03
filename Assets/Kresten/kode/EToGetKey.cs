936ba45470ea89cf92d78a136b771d4d184fb9d7
mixelmaster61 <>
2023-08-03 09:59:14 +0200

    fuck

---
+++ Assets/Kresten/kode/EToGetKey.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EToGetKey : MonoBehaviour
+   
+{ public GameObject Key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Kresten, lav et tag til din box der skal trigger
+        // som hedder "TriggerBox"
+        if (other.tag == "TriggerBox")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Key.SetActive(true);
+            }
+        }
+    }
+}
